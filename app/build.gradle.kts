e9bd693acff60bcd7d4d61393c49dfa678c27e08
kkoomming.aos <kkoomming.aos@gmail.com>
2024-07-31 17:24:18 +0900

    feat: navigation, screen 생성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,8 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.fragment)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.io.coil.compose)
 
     // Test
     testImplementation(libs.junit)
---
+++ app/src/main/java/com/hajong/blackboard/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.hajong.blackboard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hajong.blackboard.compose.BlackboardApp
+import com.hajong.blackboard.compose.ui.theme.BlackboardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BlackboardTheme {
+                BlackboardApp()
+            }
+        }
+    }
+}
